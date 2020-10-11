2c80593e467fd1faa2067f8b73b84109825dc5dc
Bobby Johnson <rjohnson19@gmail.com>
2020-10-11 15:07:28 -0400

    Add enforced checkstyle and fix pre-existing violations.

--- esd-api/src/main/java/com/rjohnson19/esdapi/DashboardApiApplication.java
+++ esd-api/src/main/java/com/rjohnson19/esdapi/DashboardApiApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DashboardApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DashboardApiApplication.class, args);
-	}
+    /**
+     * Application entry point.
+     * @param args Command line arguments.
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(DashboardApiApplication.class, args);
+    }
 
 }
