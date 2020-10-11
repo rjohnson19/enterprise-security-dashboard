33d957e64e50d2b07bc0f78ae958affcd3e83fb6
Bobby Johnson <rjohnson19@gmail.com>
2020-10-11 14:37:49 -0400

    Initializing backend project using https://start.spring.io/

---
+++ esd-api/src/main/java/com/rjohnson19/esdapi/EntepriseSecurityDashboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.rjohnson19.esdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntepriseSecurityDashboardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntepriseSecurityDashboardApiApplication.class, args);
+	}
+
+}
