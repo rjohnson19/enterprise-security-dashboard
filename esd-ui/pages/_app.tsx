443daff648636ad946457ac83c82a90c5d1ff351
Bobby Johnson <rjohnson19@gmail.com>
2020-10-12 09:30:03 -0400

    Add semantic-ui-react 2.0.0 and CSS.

--- esd-ui/pages/_app.tsx
+++ esd-ui/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import 'semantic-ui-css/semantic.min.css';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
