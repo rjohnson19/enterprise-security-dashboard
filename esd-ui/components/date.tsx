38aa4cacdd2c5479fef46148b57cf768b1ae371b
Bobby Johnson <rjohnson19@gmail.com>
2020-10-11 21:47:20 -0400

    Converted layout to tsx; tweak eslint and fix errors.

--- esd-ui/components/date.tsx
+++ esd-ui/components/date.tsx
@@ -2,7 +2,7 @@ import { parseISO, format } from 'date-fns';
 
 export default function Date({ dateString }: IDateProps): JSX.Element {
   const date: Date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
+  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
 }
 
 export interface IDateProps {
