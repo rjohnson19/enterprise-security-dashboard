b3a008510ad94ce1ab9cee64f3da5ee80a17439d
Bobby Johnson <rjohnson19@gmail.com>
2020-10-11 20:39:31 -0400

    Add a type to date

--- esd-ui/components/date.tsx
+++ esd-ui/components/date.tsx
@@ -1,7 +1,7 @@
 import { parseISO, format } from 'date-fns';
 
 export default function Date({ dateString }: IDateProps) {
-  const date = parseISO(dateString);
+  const date: Date = parseISO(dateString);
   return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
 }
 
